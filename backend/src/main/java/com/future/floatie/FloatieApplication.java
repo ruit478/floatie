d7142a7698015722063b159079503705f257da4d
Rui Teixeira <ruit478@gmail.com>
2026-05-08 17:42:50 +0100

    Fixed Application and Database connectivity

--- backend/src/main/java/com/future/floatie/FloatieApplication.java
+++ backend/src/main/java/com/future/floatie/FloatieApplication.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @RestController
 public class FloatieApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(FloatieApplication.class, args);
 	}
 
